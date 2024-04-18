d3bd11240b0f74c9140607edf66870dc612401a0
KameshTripathi <120265582+KameshTripathi@users.noreply.github.com>
2024-04-19 00:49:33 +0530

    c

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,28 +1,12 @@
-import React, { useState } from 'react'
-import { Modal } from 'antd'
-import Layout from './../components/layouts/Layout'
+import React from "react";
+import Layout from "./../components/Layout/Layout";
 
 const HomePage = () => {
-  const [showModal, setShowModal] = useState(false);
   return (
     <Layout>
-      <div className="filters">
-        <div>Range filters</div>
-        <div>
-          <button className="btn btn-primary" onclick={() => setShowModal(true)}>Add new</button>
-        </div>
-      </div>
-      <div className="content"></div>
-      <Modal
-        title="Add Transaction"
-        visible={showModal}
-        onCancel={() => setShowModal(false)}
-        footer={false}
-      >
-        <h1>hello</h1>
-      </Modal>
-    </Layout >
-  )
-}
+      <h1>Home Page</h1>
+    </Layout>
+  );
+};
 
-export default HomePage
+export default HomePage;
