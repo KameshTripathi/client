67177ac73acd053d78a91e409638ac3990aa6461
KameshTripathi <120265582+KameshTripathi@users.noreply.github.com>
2024-11-19 20:05:49 +0530

    c

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const footer = () => {
   return (
     <div className='bg-dark text-light p-4'>
-      <h6 className='text-center'>All rights reserved &copy; Kamesh Tripathi</h6>
+      <h6 className='text-center'>All rights reserved &copy; Anurag Vardhan</h6>
     </div>
   )
 }
